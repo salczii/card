30be094d19c67c8490259d7d36f5841f41ed5c32
Kacper Salek <kacper.salek32@gmail.com>
2022-02-02 10:15:08 +0100

    recreate nest project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
